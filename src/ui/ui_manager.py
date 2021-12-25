91240068ab4ba1bdc37de4f6955f47fb1fb49aac
Vladimir Makaev <vmakaev@gmail.com>
2021-12-25 16:25:49 +0000

    Allow to play online

--- src/ui/ui_manager.py
+++ src/ui/ui_manager.py
@@ -158,9 +158,14 @@ class UiManager():
         Logger.debug("Wait for Play button")
         while 1:
             img = self._screen.grab()
-            found_btn_off = self._template_finder.search(["PLAY_BTN", "PLAY_BTN_GRAY"], img, roi=self._config.ui_roi["offline_btn"], threshold=0.8, best_match=True)
-            found_btn_on = self._template_finder.search(["PLAY_BTN", "PLAY_BTN_GRAY"], img, roi=self._config.ui_roi["online_btn"], threshold=0.8, best_match=True)
-            found_btn = found_btn_off if found_btn_off.valid else found_btn_on
+            # the template finder can be used to search for a specific template, in this case the play btn.
+            # it returns a bool value (True or False) if the button was found, and the position of it
+            # roi = Region of interest. It reduces the search area and can be adapted within game.ini
+            # by running >> python src/screen.py you can visualize all of the currently set region of interests
+            found_btn = self._template_finder.search(["PLAY_BTN", "PLAY_BTN_GRAY"], img, roi=self._config.ui_roi["online_btn"], threshold=0.8, best_match=True)
+            if found_btn.name == "PLAY_BTN_GRAY":
+                wait(1, 5)
+                break
             if found_btn.name == "PLAY_BTN":
                 x, y = self._screen.convert_screen_to_monitor(found_btn.position)
                 Logger.debug(f"Found Play Btn")
